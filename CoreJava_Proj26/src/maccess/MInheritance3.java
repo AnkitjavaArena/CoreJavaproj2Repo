407040851e4846d5bfb88d1d700de6e928dbdc77
Ankit <ankit.scrps@gmail.com>
2022-08-19 12:29:23 +0530

    Added project26

---
+++ CoreJava_Proj26/src/maccess/MInheritance3.java
@@ -0,0 +1,12 @@
+package maccess;
+
+import test.IClass;
+
+public class MInheritance3 {
+	public static void main(String[] args) {
+		IClass ob=new IClass();
+		ob.m1(12);
+		ob.m2(13);
+		ob.m3(14);
+	}//end of main method
+}//end of class
---
+++ CoreJava_Proj26/src/test/IClass.java
@@ -0,0 +1,15 @@
+package test;
+
+public class IClass implements ITest3{
+	public void m1(int x) {
+		System.out.println("x::"+x);
+	}//end of method
+	
+	public void m2(int y) {
+		System.out.println("y::"+y);
+	}//end of method
+	
+	public void m3(int z) {
+		System.out.println("z::"+z);
+	}//end of method
+}//end of class
