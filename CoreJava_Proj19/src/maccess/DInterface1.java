3aee999fa60d1ac84cc77f4de59b5cc547d70219
Ankit <ankit.scrps@gmail.com>
2022-08-19 12:06:15 +0530

    Added project19

---
+++ CoreJava_Proj19/src/maccess/DInterface1.java
@@ -0,0 +1,9 @@
+package maccess;
+
+import test.ITest;
+
+public class DInterface1 {
+	public static void main(String[] args) {
+		System.out.println("The value a::"+ITest.a);
+	}//end of main
+}//end of class
---
+++ CoreJava_Proj19/src/test/ITest.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface ITest {
+	public static final int a=20;
+}//end of interface
