b16bf7f546744747b72093c1fea2fa18f7a163b5
Ankit <ankit.scrps@gmail.com>
2022-08-19 12:28:20 +0530

    Added project25

---
+++ CoreJava_Proj25/src/maccess/MInheritance2.java
@@ -0,0 +1,9 @@
+package maccess;
+import test.*;
+public class MInheritance2 {
+	public static void main(String[] args) {
+		SubClass ob=new SubClass();
+		ob.m1(123);
+		ob.m2(234);
+	}//end of method
+}//end of class
---
+++ CoreJava_Proj25/src/test/SubClass.java
@@ -0,0 +1,7 @@
+package test;
+
+public class SubClass extends PClass implements ITest {
+	public void m2(int y) {
+		System.out.println("y::"+y);
+	}//end of method
+}//end of class
