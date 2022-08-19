1cbb00bec5ca346ee39be36b5723d175203ce053
Ankit <ankit.scrps@gmail.com>
2022-08-19 12:27:33 +0530

    Added project24

---
+++ CoreJava_Proj24/src/maccess/MInheritance1.java
@@ -0,0 +1,9 @@
+package maccess;
+import test.*;
+public class MInheritance1 {
+	public static void main(String[] args) {
+		IClass ob=new IClass();
+		ob.m1(123);
+		ob.m2(234);
+	}//end of method
+}//end of class
