c38fd62d6e42e029c41fd00d4cd177e1500f0887
Ankit <you@example.com>
2022-12-09 11:23:19 +0530

    new Commit

--- CoreJava_Proj01/src/pack01/HelloMessage.java
+++ CoreJava_Proj01/src/pack01/HelloMessage.java
@@ -3,5 +3,6 @@ package pack01;
 public class HelloMessage {
 public static void main(String[] args) {
 	System.out.println("Hello World....");
+	System.out.println("Hello World....");
 }//end of main
 }//end of class
